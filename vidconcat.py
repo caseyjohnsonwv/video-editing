19147a8689581176baf2a5ec2cdcee6d34067f55
Casey Johnson <ctj0001@mix.wvu.edu>
2019-03-31 20:46:13 -0400

    added flag for black cuts between clips

--- vidconcat.py
+++ vidconcat.py
@@ -2,17 +2,39 @@ from moviepy.editor import *
 import os
 import sys
 
+
+"""
+ARGUMENT PARSING FUNCTION
+"""
+
+def parse_arg(arg, rejections=[], default=None):
+	arg = arg.lower()
+	for test in rejections:
+		if (arg == test):
+			return default
+	return arg
+	
+	
+	
+"""
+BEGIN MAIN SCRIPT
+"""
+
 #parse args
-if (len(sys.argv) != 3):
-	print("\nERROR: Expected 'vidconcat.py <src_dir> <dest_file>'")
+if (len(sys.argv) != 4):
+	print("\nERROR: Expected 'vidconcat.py <src_dir> <dest_file> <black_cuts?>'")
 	exit()
+	
+rejs = ['no', 'none', 'n', 'f', 'false']
 input_dir_name = sys.argv[1]
 output_file = sys.argv[2]
+black_cuts = False if not parse_arg(sys.argv[3], rejections=rejs, default=False) else True
 
 #concat setup
 total_time = 0
 clip = None
 clip_list = []
+blackscreen = ColorClip(size=(1920, 1080), color=(0,0,0), duration=1.0)
 
 #open every clip
 input_dir = os.getcwd() + "/" + input_dir_name
@@ -29,14 +51,21 @@ for filename in os.listdir(input_dir):
 	total_time += clip.duration
 	
 	print("%i: %s (%.1f sec)" % (clip_num, filename, clip.duration))
+	
+	#add black screen after every clip, if desired
+	if (black_cuts):
+		clip_list.append(blackscreen)
 
 #add black screens to start/end
-clip_list.insert(0, ColorClip(size=(1920, 1080), color=(0,0,0), duration=1.0))
-clip_list.append(ColorClip(size=(1920, 1080), color=(0,0,0), duration=1.0))
+clip_list.insert(0, blackscreen)
+if (not black_cuts):
+	clip_list.append(blackscreen)
 
 #concat clips
 final_cut = concatenate_videoclips(clip_list)
 del clip_list
+del blackscreen
+del clip
 
 #export video
 print("---\nFinal video length: {0:.1f} sec.\n---".format(final_cut.duration))
